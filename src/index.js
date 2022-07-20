df8941443c6adfc8a0f0598f0e9fc853a4531509
Avery Yang <averyyang2674@gmail.com>
2022-07-20 09:55:12 -0500

    practice generator functions: generator fx, instance, console.log

--- src/index.js
+++ src/index.js
@@ -7,6 +7,52 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+//! 1. myGenerator generator
+function* myGenerator() {
+    try {
+        yield true; //boolean
+        yield 100; //number
+        yield 'Hello!'; //string
+        yield [1, 2, 3]; //array
+        yield { name: 'Avery' }; //object
+    } catch (error) {
+        console.log(`ERR in myGenerator`, error);
+    }
+}
+
+//! 2. Create instance
+const goDogGo = myGenerator();
+
+//! 3. call generator with .next()
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+console.log(goDogGo.next().value);
+
+//! 4. getSwitch generator
+function* getSwitch() {
+    try {
+        while (true) {
+            yield 'on';
+            yield 'off';
+        }
+    } catch (err) {
+        console.log(`ERR in getSwitch`, err);
+    }
+}
+
+//! 5. create instance
+const toggle = getSwitch();
+
+//! 6. call generator
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+console.log(toggle.next().value);
+
 const elementList = (state = [], action) => {
     switch (action.type) {
         case 'SET_ELEMENTS':
@@ -14,13 +60,34 @@ const elementList = (state = [], action) => {
         default:
             return state;
     }
-};    
+};
 
-// this is the saga that will watch for actions
-function* watcherSaga() {
-
+//! 6. countDown generator
+// Generators can have loops, conditional statements, variables and even return (just be careful about return! That still will stop!).
+function* countDownGenerator() {
+    let a = 10;
+    while (a > 0) {
+        yield `Launching in ${a}`;
+        a -= 1;
+    }
+    yield `Take off!`;
 }
 
+const countDown = countDownGenerator();
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+console.log(countDown.next().value);
+
+// this is the saga that will watch for actions
+function* watcherSaga() {}
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -32,15 +99,15 @@ const storeInstance = createStore(
     combineReducers({
         elementList,
     }),
-    applyMiddleware(sagaMiddleware, logger),
+    applyMiddleware(sagaMiddleware, logger)
 );
 
 sagaMiddleware.run(watcherSaga);
 
 ReactDOM.render(
     <Provider store={storeInstance}>
-        <App/>
-    </Provider>, 
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
 
